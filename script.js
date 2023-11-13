19b408789cf12d475668f43a2ef722c2fa3b2e9a
EmmaAmos <elovestodraw@gmail.com>
2023-11-13 16:59:29 -0500

    got things working

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-//for image scrolling
-document.addEventListener("DOMContentLoaded", function() {
-    const slider = document.querySelector('.slider');
-    const prevBtn = document.getElementById('prevBtn');
-    const nextBtn = document.getElementById('nextBtn');
+/*
+function before(){
+    document.getElementById('myimage').
+        src="images/titleCard.jpg";
+}
+function next(){
+    document.getElementById('myArray')
+    scr=["images/titleCard.jgp", "images/bees.jpg", "images/AboveDisneySprings.png"]
+}
+*/
 
-    let currentImageIndex = 0;
-
-    function showImage(index) {
-        slider.style.transform = `translateX(${-index * 100}%)`;
-    }
-
-    function showNextImage() {
-        currentImageIndex = (currentImageIndex + 1) % slider.children.length;
-        showImage(currentImageIndex);
-    }
-
-    function showPrevImage() {
-        currentImageIndex = (currentImageIndex - 1 + slider.children.length) % slider.children.length;
-        showImage(currentImageIndex);
-    }
-
-    nextBtn.addEventListener('click', showNextImage);
-    prevBtn.addEventListener('click', showPrevImage);
+$(document).ready(function(){
+    var idx = 0;
+    var interval = 6000;
+    var images = $('ul.slider li img');
+    setInterval(function(){
+        idx++;
+        $('img#current').fadeOut(function () { 
+            $(this).attr('src', $(images[idx%images.length]).attr('src')).fadeIn()                
+        });
+    }, interval);
 });
-//end of scrolling
